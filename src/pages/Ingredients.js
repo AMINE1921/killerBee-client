7a2e8fb8c1c2ee81e6261c6c391638d707dd503d
AMINE1921 <amineoubaya1921@gmail.com>
2022-09-25 19:12:46 +0200

    add data ingredients & processes

--- src/pages/Ingredients.js
+++ src/pages/Ingredients.js
@@ -1,9 +1,43 @@
-import React from "reactn";
+import React, {useEffect, useState} from "reactn";
+import TableFlex from "../components/Table/TableFlex";
+import { getListIngredient, deleteIngredient } from "../utils/api/ingredient";
+import toastNotif from "../utils/toastNotif";
 
 const Ingredients = () => {
+  const [listIngredients, setListIngredients] = useState([]);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = () => {
+    getListIngredient()
+      .then((response) => {
+        if (response?.success) {
+          setListIngredients(response?.ingredients);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteItem = (id) => {
+    deleteIngredient(id)
+      .then((response) => {
+        if (response.success) {
+          getList();
+          toastNotif("L'élément a été supprimé !", "success");
+        }
+      })
+      .catch((error) => {
+        toastNotif(error?.data?.message, "error");
+      });
+  };
+
   return (
     <div>
-      <h1>Ingredients</h1>
+      <TableFlex page="ingredients" data={listIngredients} deleteItem={deleteItem} />
     </div>
   );
 };
